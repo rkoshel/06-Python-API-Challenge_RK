550995aef208e59b0c660158404aac3dce182f1e
Roman Koshel <koshel.roman@gmail.com>
2020-10-16 01:31:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 06-Python-API-Challenge_RK
+ 
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "cf2cf4c53541ae7442f7692612047e0a"
+
+# Google API Key
+g_key = "AIzaSyCyswWXD3I9m23ubhC3BW1ODkorvKbIz4g"
